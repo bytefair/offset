0d942618bd4cf45a3a0efa24a6ac5cee21fe669c
Paul Graham <paul@bytefair.com>
2014-01-01 21:27:21 -0600

    adds some configurable paths for grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,19 @@
 module.exports = function (grunt) {
+
 	// auto task loading
 	require('load-grunt-tasks')(grunt);
+
 	// spits out the time the tasks take
 	require('time-grunt')(grunt);
 
 	grunt.initConfig({
+
+		// configurable paths
+		paths: {
+			app:  'app',
+			dist: 'dist'
+		}
+
 	});
 
 	grunt.registerTask('default', [
