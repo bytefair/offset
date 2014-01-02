5dbd4cb288005e1ea5f7bf42805a133d2c9d7529
Paul Graham <paul@bytefair.com>
2014-01-01 21:19:28 -0600

    gruntfile skeleton

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function (grunt) {
+	// auto task loading
+	require('load-grunt-tasks')(grunt);
+	// spits out the time the tasks take
+	require('time-grunt')(grunt);
+
+	grunt.initConfig({
+	});
+
+	grunt.registerTask('default', [
+	]);
+};
