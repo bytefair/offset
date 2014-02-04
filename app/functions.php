ac323e9c11b1de937d6f7315be021c574bbee5fb
Paul Graham <pgraham@cordelllaw.com>
2014-02-04 13:29:31 -0600

    new wrapping for widgets

--- app/functions.php
+++ app/functions.php
@@ -1,19 +1,19 @@
 <?php
-
+// framework includes
 include_once('lib/navigation.php');
 
 add_action( 'widgets_init', 'offset_register_sidebars' );
 function offset_register_sidebars()
 {
-	register_sidebar(array(
+	register_sidebar( array(
 		'id'          => 'sidebar-default',
 		'name'        => __( 'Default Sidebar', 'offset' ),
 		'description' => __( 'The primary site sidebar.', 'offset' ),
-		// 'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		// 'after_widget' => '</div>',
-		// 'before_title' => '<h4 class="widgettitle">',
-		// 'after_title' => '</h4>',
-	));
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
 }
 
 add_action( 'wp_enqueue_scripts', 'offset_load_styles' );
