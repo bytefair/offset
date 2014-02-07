45f20c975d0efe6c2212f4ec0f96e21207529911
Paul Graham <pgraham@cordelllaw.com>
2014-02-07 11:46:20 -0600

    future hook for functions that should be fired on init hook

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 // framework includes
+require_once locate_template( 'lib/init.php' );
 require_once locate_template( 'lib/navigation.php' );
 require_once locate_template( 'lib/widgets.php' );
 require_once locate_template( 'lib/scripts.php' );
