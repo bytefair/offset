e2e3dd5aab41306b44e280aa0f8850317f6e0835
Paul Graham <pgraham@cordelllaw.com>
2014-02-07 17:16:46 -0600

    lots of new init stuff

--- functions.php
+++ functions.php
@@ -1,7 +1,16 @@
 <?php
-// framework includes
-require_once locate_template( 'lib/init.php' );
-require_once locate_template( 'lib/navigation.php' );
+/**
+ * functions.php
+ *
+ * Shoving everything in a flat file was and remains hella dumb. Please review
+ * the README if you're confused about how this works. Another solution that's a
+ * bad idea is to create giant 20 line comment blocks to set apart sections. Not
+ * respecting vertical space is bad manners. - PG
+ */
+
+//* comments specify where code fires - http://codex.wordpress.org/Plugin_API/Action_Reference
+require_once locate_template( 'lib/init.php' );       // after_setup_theme
+require_once locate_template( 'lib/navigation.php' ); //
 require_once locate_template( 'lib/widgets.php' );
 require_once locate_template( 'lib/scripts.php' );
 require_once locate_template( 'lib/tweaks.php' );
---
+++ lib/init.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * setup-theme.php
+ *
+ * Initial setup. Things that must be fired upon the first action hook.
+ */
+
+function offset_setup()
+{
+	// translation
+	load_theme_textdomain( 'offset', get_template_directory() . '/languages' );
+
+	// http://codex.wordpress.org/Function_Reference/register_nav_menus
+	register_nav_menus( array(
+		'main_navigation' => __( 'Main Navigation', 'offset' )
+	) );
+
+	// http://codex.wordpress.org/Function_Reference/add_theme_support
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'automatic-feed-links' );
+	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form' ) );
+	// add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
+	// add_theme_support( 'custom-background' );
+	// add_theme_support( 'custom-header' );
+
+	// you can style the text inside TinyMCE and you should
+	// add_editor_style();
+
+}
+add_action( 'after_setup_theme', 'offset_setup' );
