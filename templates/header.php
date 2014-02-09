b05d2c78f70b97d6ceed7b78527c426d4d324e24
Paul Graham <paul@bytefair.com>
2014-02-09 12:07:44 -0600

    role="document" on site wrapper

--- templates/header.php
+++ templates/header.php
@@ -7,7 +7,7 @@ get_template_part( 'head.php' ); ?>
 
 
 <body <?php body_class(); ?>>
-	<div class="site__wrapper">
+	<div class="site__wrapper" role="document">
 
 
 		<header role="banner" class="site__header">
