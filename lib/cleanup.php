25f86eaae646d5a54870595b95c002cccfa6b237
Paul Graham <pgraham@cordelllaw.com>
2014-02-10 17:35:12 -0600

    cleans up body_class()

--- lib/cleanup.php
+++ lib/cleanup.php
@@ -71,3 +71,23 @@ function offset_rel_canonical()
 	echo "\t<link rel=\"canonical\" href=\"$link\">\n";
 }
 add_action('init', 'offset_head_cleanup');
+
+
+//* clean up body_class() ouput (from Roots)
+function offset_body_class( $classes ) {
+	// Add post/page slug
+	if (is_single() || is_page() && !is_front_page()) {
+		$classes[] = basename(get_permalink());
+	}
+
+	// Remove unnecessary classes
+	$home_id_class = 'page-id-' . get_option('page_on_front');
+	$remove_classes = array(
+		'page-template-default',
+		$home_id_class
+	);
+	$classes = array_diff($classes, $remove_classes);
+
+	return $classes;
+}
+add_filter('body_class', 'offset_body_class');
