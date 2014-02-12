533c10cde65000b88e5fafdb2710ae606192818d
Paul Graham <pgraham@cordelllaw.com>
2014-02-12 16:53:47 -0600

    sets up inuit for styling with some defaults I will need

--- lib/scripts.php
+++ lib/scripts.php
@@ -2,6 +2,6 @@
 
 function offset_load_styles()
 {
-	wp_enqueue_style( 'offset-styles', get_stylesheet_uri() );
+	wp_enqueue_style( 'master', get_stylesheet_directory_uri() . '/assets/master.css' );
 }
 add_action( 'wp_enqueue_scripts', 'offset_load_styles' );
