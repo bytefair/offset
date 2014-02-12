584bfb5c82d6fe557de8d313c3868754a1647bc8
Paul Graham <pgraham@cordelllaw.com>
2014-02-12 10:51:40 -0600

    first go at single post processing

---
+++ single.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * single.php shows up when you view any post by itself.
+ */
+
+get_template_part( 'templates/header', get_post_format() ); ?>
+
+
+<main class="content-pane single"><?php
+
+		while ( have_posts() ) :
+			the_post();
+			get_template_part( 'templates/content', get_post_format() );
+		endwhile; ?>
+
+</main><?php
+
+
+get_template_part( 'templates/sidebar', get_post_format() );
+get_template_part( 'templates/footer', get_post_format() );
---
+++ templates/content-single.php
@@ -0,0 +1,31 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'hentry' ); ?>>
+	<header class="article__header">
+		<h1 class="article__title entry-title"><?php the_title(); ?></h1>
+		<div class="article__meta byline author vcard">
+			<?php get_template_part( 'templates/article_meta', get_post_format() ); ?>
+		</div>
+	</header>
+
+
+		<?php if ( is_archive() || is_search() ) : ?>
+			<div class="article__excerpt">
+				<?php the_excerpt(); ?>
+			</div>
+		<?php else : ?>
+			<div class="article__content">
+				<?php the_content(); ?>
+			</div>
+		<?php endif; ?>
+
+
+	<footer class="article__footer">
+		<p class="article__categories">
+			<?php _e( 'Categorized: ', 'offset' ); ?><?php the_category(', '); ?>
+		</p>
+		<p class="article__tags">
+			<?php the_tags(); ?>
+		</p>
+		<?php wp_link_pages( array( 'before' => '<nav class="page-nav"><p>' . __( 'Pages:', 'offset' ), 'after' => '</p></nav>' ) ); ?>
+	</footer>
+	<?php comments_template(); ?>
+</article>
