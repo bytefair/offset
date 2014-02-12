9368d0b6722e18fa3de8c70e986465d1948a5d3a
Paul Graham <pgraham@cordelllaw.com>
2014-02-12 17:06:17 -0600

    insert blog name in titles

--- templates/head.php
+++ templates/head.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<title><?php wp_title( '|', true, 'right' ); echo bloginfo( 'name' ); ?></title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
