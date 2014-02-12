0e7910100a6aa8027ebc2766743b4a3d1868e789
Paul Graham <pgraham@cordelllaw.com>
2014-02-12 10:50:00 -0600

    small error change

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,7 +1,7 @@
 <div role="complementary" class="sidebar"><?php
 	if ( ! dynamic_sidebar( 'sidebar-default' ) ) :
 		if ( WP_DEBUG ) {
-			echo '<div class="offset__notice">' . esc_html__( 'DEBUG NOTICE: Sidebar is being called with no widgets. Add widgets or customize sidebar.php.', 'offset' ) . '</div>';
+			echo '<div class="offset__notice">' . esc_html__( 'DEBUG NOTICE: Sidebar is being called with no widgets. Add widgets, ban sidebar from specific templates at lib/define.php, or customize sidebar.php.', 'offset' ) . '</div>';
 		}
 	endif; ?>
 </div>
