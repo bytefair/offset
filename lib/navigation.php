894946de5b16fb5ebb2e05652c4f30af91147a87
Paul Graham <paul@bytefair.com>
2014-02-16 01:44:33 -0600

    eliminates a prefix slash on pagination URLs

--- lib/navigation.php
+++ lib/navigation.php
@@ -6,7 +6,8 @@
  */
 
 // http://codex.wordpress.org/Function_Reference/register_nav_menus
-function offset_menu_init() {
+function offset_menu_init()
+{
 	register_nav_menus( array(
 		'main-navigation'   => __( 'Main Navigation', 'offset' )
 	) );
@@ -23,16 +24,16 @@ function offset_blog_pagination()
 		echo '<nav class="pagination">';
 
 		$current_page = max( 1, get_query_var( 'paged' ) );
-		echo paginate_links(array(
+		echo paginate_links( array(
 			'base'      => get_pagenum_link(1) . '%_%',
-			'format'    => '/page/%#%',
+			'format'    => 'page/%#%',
 			'current'   => $current_page,
 			'total'     => $total_pages,
 			'prev_text' => '&larr;&nbsp;Previous',
 			'next_text' => 'Next&nbsp;&rarr;',
 			'end_size'  => 3,
 			'mid_size'  => 3
-		));
+		) );
 		echo '</nav>';
 	}
 }
