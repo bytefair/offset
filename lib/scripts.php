872fde19d9b110237d805f8177361903a51181c4
Paul Graham <paul@bytefair.com>
2014-02-16 01:43:59 -0600

    fixes CSS enqueue

--- lib/scripts.php
+++ lib/scripts.php
@@ -7,6 +7,6 @@
 
 function offset_load_styles()
 {
-	wp_enqueue_style( 'master', get_stylesheet_directory_uri() . '/assets/master.css' );
+	wp_enqueue_style( 'master', get_stylesheet_directory_uri() . '/dist/css/master.css' );
 }
 add_action( 'wp_enqueue_scripts', 'offset_load_styles' );
