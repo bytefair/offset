0c7b2a1faa735cb0abecd7de62ccc4680ade61db
Paul Graham <pgraham@cordelllaw.com>
2014-02-18 11:34:42 -0600

    adds comment to clarify functionality

--- lib/scripts.php
+++ lib/scripts.php
@@ -7,6 +7,7 @@
 
 function offset_load_styles()
 {
-	wp_enqueue_style( 'master', get_stylesheet_directory_uri() . '/dist/css/master.css' );
+	// refers to dev stylesheet, rename reference to .min.css for production
+	wp_enqueue_style( 'master', get_stylesheet_directory_uri() . '/dist/css/master.css', false );
 }
 add_action( 'wp_enqueue_scripts', 'offset_load_styles' );
