4c4a34b930e3b5e2a0767f2890025dba07de127e
Paul Graham <paul@bytefair.com>
2014-03-07 00:50:41 -0600

    page template reflects how the rest should be organized

--- page.php
+++ page.php
@@ -9,19 +9,12 @@
  * @license http://opensource.org/licenses/MIT
  * @since 0.1.0
  */
-
-get_template_part( 'templates/header' ); ?>
-
+?>
 
 <main class="content-pane"><?php
-
 	while ( have_posts() ) :
 		the_post();
 		the_content();
 	endwhile;
 	wp_link_pages( array( 'before' => '<nav class="pagination">', 'after' => '</nav>' ) ); ?>
-
-</main><?php
-
-
-get_template_part( 'templates/footer' );
+</main>
