f26f93296ae95a8cea8f007ab2fb67f96a50c2a0
Paul Graham <pgraham@coolfire.com>
2014-03-23 16:55:05 -0500

    a bit of final cleanup

--- lib/scripts.php
+++ lib/scripts.php
@@ -10,7 +10,7 @@ function offset_load_styles()
 	// refers to dev stylesheet, rename reference to .min.css for production
 	wp_enqueue_style( 'master', get_stylesheet_directory_uri() . '/css/master.css', false );
 
-	wp_register_script( 'footer_scripts', get_stylesheet_directory_uri() . '/dist/footer.min.js', array('jquery') );
+	wp_register_script( 'footer_scripts', get_stylesheet_directory_uri() . '/javascript/footer-ck.js', array('jquery') );
 
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'footer_scripts' );
