1f71c341b7c47a728c5f9cb8166a5bb1d7f6bf38
Paul Graham <paul@bytefair.com>
2014-05-23 01:06:02 -0500

    simplifies inclusions

--- functions.php
+++ functions.php
@@ -11,11 +11,10 @@
  * @since 0.1.0
  */
 
-//  for an explanation of why we use require_once locate_template() see https://github.com/roots/roots/pull/179
-require_once locate_template( 'lib/define.php' );
-require_once locate_template( 'lib/init.php' );       // after_setup_theme
-require_once locate_template( 'lib/navigation.php' );
-require_once locate_template( 'lib/widgets.php' );    // widgets_init
-require_once locate_template( 'lib/scripts.php' );    // wp_enqueue_scripts
-require_once locate_template( 'lib/cleanup.php' );
-require_once locate_template( 'lib/tweaks.php' );
+locate_template( 'lib/define.php',     true, true );
+locate_template( 'lib/init.php',       true, true );
+locate_template( 'lib/navigation.php', true, true );
+locate_template( 'lib/widgets.php',    true, true );
+locate_template( 'lib/scripts.php',    true, true );
+locate_template( 'lib/cleanup.php',    true, true );
+locate_template( 'lib/tweaks.php',     true, true );
