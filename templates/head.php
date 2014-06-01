da9092605c48400c1f3624582042e14541da29a9
Paul Graham <paul@bytefair.com>
2014-05-31 22:40:34 -0500

    show the blog name in the title

--- templates/head.php
+++ templates/head.php
@@ -15,7 +15,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<title><?php wp_title('|', true, 'right'); ?></title>
+	<title><?php wp_title('|', true, 'right') . bloginfo('name'); ?></title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
