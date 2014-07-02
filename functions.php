4baaddc882c54049d382bcc8c93b2d0a8e782cf7
Paul Graham <pgraham@coolfire.com>
2014-07-02 11:29:39 -0500

    adds is_tree() conditional function

--- functions.php
+++ functions.php
@@ -14,10 +14,11 @@
 /**
  * Required WP Setup
  */
-locate_template('lib/define.php',   true, true); // Define settings
-locate_template('lib/supports.php', true, true); // Set theme supports
-locate_template('lib/menus.php',    true, true); // Set up menus
-locate_template('lib/widgets.php',  true, true); // Set up sidebars/widgets
-locate_template('lib/assets.php',   true, true); // Queue JS and CSS
-locate_template('lib/cleanup.php',  true, true); // Cleanups and tweaks to WP
-locate_template('lib/wysiwyg.php',  true, true); // WYSIWYG customizations
+locate_template('lib/define.php',     true, true); // Define settings
+locate_template('lib/supports.php',   true, true); // Set theme supports
+locate_template('lib/menus.php',      true, true); // Set up menus
+locate_template('lib/widgets.php',    true, true); // Set up sidebars/widgets
+locate_template('lib/assets.php',     true, true); // Queue JS and CSS
+locate_template('lib/cleanup.php',    true, true); // Cleanups and tweaks to WP
+locate_template('lib/wysiwyg.php',    true, true); // WYSIWYG customizations
+locate_template('lib/functions.php'), true, true); // Custom functions
---
+++ lib/functions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * wysiwyg.php
+ *
+ * wysiwyg.php contains addons and customizations to the MCE editor in the
+ * WordPress backend.
+ *
+ * @package Offset
+ * @author Paul Graham <paul@bytefair.com>
+ * @license http://opensource.org/licenses/MIT
+ * @since 0.7.0
+ */
+
+/**
+ * Conditional function to tell if a page is or is a child of a particular ID
+ *
+ * @since 0.7.0
+ */
+function is_tree($pid)
+{
+	global $post;
+	if( is_page() && ($post->post_parent==$pid || is_page($pid)) )
+		return true;
+	else
+		return false;
+};
