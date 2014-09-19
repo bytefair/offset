f4ec750e16db71635ebbd1da83060480daa289f5
Paul Graham <pgraham@coolfire.com>
2014-09-19 01:26:10 -0500

    tabbing issue

--- 404.php
+++ 404.php
@@ -15,15 +15,15 @@ locate_template('templates/header', true, true); ?>
 
 <main>
 
-		<header class="article__header">
-			<h1 class="article__title"><?php _e('404 Not Found', 'offset'); ?></h1>
-		</header>
+	<header class="article__header">
+		<h1 class="article__title"><?php _e('404 Not Found', 'offset'); ?></h1>
+	</header>
 
-		<div class="article__content">
-			<p><?php _e('Nothing was found at this URL. Try searching for your page below.', 'offset'); ?></p>
-		</div><?php
+	<div class="article__content">
+		<p><?php _e('Nothing was found at this URL. Try searching for your page below.', 'offset'); ?></p>
+	</div><?php
 
-		get_search_form(); ?>
+	get_search_form(); ?>
 
 </main><?php
 
