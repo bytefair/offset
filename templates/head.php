9a4d9b23c59d2da5d31da36302cd877b80571d46
Paul Graham <paul@bytefair.com>
2015-05-05 17:44:56 -0500

    kill stupid header functions

--- templates/head.php
+++ templates/head.php
@@ -19,9 +19,5 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<?php /* pingback sucks but it's standard and no self-pings are on by default */ ?>
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
 </head>
